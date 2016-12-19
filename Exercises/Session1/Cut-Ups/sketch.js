f20e13ce1d7f227dbae7308626d8f41441634fa7
StefanoCocomazzi <stefanococomazzi@live.it>
2016-12-19 23:43:16 +0100

    Add files via upload

---
+++ Exercises/Session1/Cut-Ups/sketch.js
@@ -0,0 +1,22 @@
+function cutUp(words,l){
+	var poem = "";
+	for(i=0;i<l;i++){
+		poem += random(words) + " ";
+	}
+	return poem;
+	
+}
+
+
+function setup(){
+	noCanvas();
+	var button = select("#cutup");
+	button.mousePressed(function(){
+		var usertext = select("#usertext").value();
+		var words = splitTokens(usertext, ' ,!.?()');
+		var plength = select("#length").value();
+		var poem = cutUp(words,plength);
+		var p=select("#poem");
+		p.html(poem);
+	});
+}
